1bdb06fb59926f4feda05270148990a9b8ba459a
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-23 20:48:01 +0700

    add links to implementation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,74 @@
-<a href="/2022/january/3-footer">2020 January 3 - Footer</a>
+<script>
+	import { Temporal, Intl } from '@js-temporal/polyfill'
+
+	const list = [
+		{
+			id: 1522,
+			title: 'Set Status',
+			date: Intl.DateTimeFormat().format(
+				Temporal.PlainDate.from({
+					year: 2022,
+					month: 1,
+					day: 4
+				})
+			),
+			linkOriginal: 'https://www.uidesigndaily.com/posts/figma-set-status-dropdown-day-1522',
+			linkComponent: '/2022/january/4-set-status'
+		},
+		{
+			id: 1521,
+			title: 'Footer',
+			date: Intl.DateTimeFormat().format(
+				Temporal.PlainDate.from({
+					year: 2022,
+					month: 1,
+					day: 3
+				})
+			),
+
+			linkOriginal: 'https://www.uidesigndaily.com/posts/figma-footer-dark-mode-day-1521',
+			linkComponent: '/2022/january/3-footer'
+		}
+	]
+</script>
+
+<div class="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+	<ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		{#each list as item}
+			<li
+				class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+			>
+				<div class="flex-1 flex flex-col p-8">
+					<img
+						class="w-32 h-32 flex-shrink-0 mx-auto rounded-full"
+						src="https://via.placeholder.com/256"
+						alt=""
+					/>
+					<h3 class="mt-6 text-gray-900 text-sm font-medium">{item.title}</h3>
+
+					<time class="mt-1 text-gray-500 text-sm">{item.date}</time>
+				</div>
+				<div>
+					<div class="-mt-px flex divide-x divide-gray-200">
+						<div class="w-0 flex-1 flex">
+							<a
+								href={item.linkOriginal}
+								class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+							>
+								UIDesignDaily.com
+							</a>
+						</div>
+						<div class="-ml-px w-0 flex-1 flex">
+							<a
+								href={item.linkComponent}
+								class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+							>
+								Implementation
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
+</div>
