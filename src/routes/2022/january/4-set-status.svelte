30a3332dbf1f2502565e29fc9c0c274ca2b64f56
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-23 20:15:36 +0700

    add Set Status 4 Jan 2022

---
+++ src/routes/2022/january/4-set-status.svelte
@@ -0,0 +1,90 @@
+<div class="bg-low-contrast grid lg:grid-cols-2 place-items-center place-content-center h-screen">
+	<div
+		class="w-[548px] h-[260px] bg-white p-8 rounded-2xl flex justify-between flex-col shadow-lg col-span-1"
+	>
+		<h2 class="font-bold text-[24px] text-high-contrast">Add colleagues</h2>
+		<input
+			type="text"
+			placeholder="Enter name, email or tag"
+			class="p-[10px] rounded-lg border border-[#dadcf5] bg-[#f8f8ff] placeholder:text-high-contrast text-lg w-full placeholder:font-medium"
+		/>
+
+		<div class="space-x-[11px] flex justify-end">
+			<button
+				class="p-[10px] border-[#dadcf5] border rounded-lg text-high-contrast text-lg font-medium"
+				>Cancel</button
+			>
+			<button class="p-[10px] bg-primary rounded-lg text-white text-lg font-medium">Add</button>
+		</div>
+	</div>
+	<div class="col-span-1 mt-20 lg:mt-0">
+		<button
+			class="rounded-lg text-high-contrast border border-primary p-[10px] inline-flex gap-3 font-medium"
+		>
+			<span class="text-lg">In a call</span>
+			<svg
+				width="15"
+				height="15"
+				viewBox="0 0 15 15"
+				fill="none"
+				class="w-6 h-6 text-high-contrast"
+			>
+				<use href="/static/icons/chevron-down.svg#chevron-down" />
+			</svg>
+		</button>
+		<div class="mt-2 bg-white p-8 divide-y divide-[#DADCF5] rounded-lg">
+			<ul class="space-y-4">
+				<li class="flex items-center gap-2">
+					<div class="rounded-full w-4 h-4 bg-[#4AC63F]" />
+					<p class="text-lg text-high-contrast font-medium">Available</p>
+				</li>
+				<li class="flex items-center gap-2">
+					<div class="rounded-full w-4 h-4 bg-[#F95555]" />
+					<p class="text-lg text-high-contrast font-medium">Busy</p>
+				</li>
+				<li class="flex items-center gap-2">
+					<div class="rounded-full w-4 h-4 bg-[#F9AD55]" />
+					<p class="text-lg text-high-contrast font-medium">Appear away</p>
+				</li>
+				<li class="flex items-center gap-2 pb-4">
+					<div class="rounded-full w-4 h-4 bg-[#9B9BC3]" />
+					<p class="text-lg text-high-contrast font-medium">Appear offline</p>
+				</li>
+			</ul>
+			<p class="flex items-center gap-2 py-4 text-lg text-high-contrast font-medium">
+				<svg
+					width="15"
+					height="15"
+					viewBox="0 0 15 15"
+					fill="none"
+					class="w-6 h-6 text-high-contrast"
+				>
+					<use href="/static/icons/clock.svg#clock" />
+				</svg>
+				<span>Duration</span>
+			</p>
+
+			<p class="flex items-center gap-2 pt-4 text-lg text-high-contrast font-medium">
+				<svg
+					width="15"
+					height="15"
+					viewBox="0 0 15 15"
+					fill="none"
+					class="w-6 h-6 text-high-contrast"
+				>
+					<use href="/static/icons/reset.svg#reset" />
+				</svg>
+				<span>Reset status</span>
+			</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	:root {
+		--color-primary: #7758ce;
+		--color-ascent: #5ac1d8;
+		--text-color-high: #161842;
+		--text-color-low: #838faa;
+	}
+</style>
