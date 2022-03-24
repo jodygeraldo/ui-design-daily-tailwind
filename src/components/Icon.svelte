a55a758e5fb992c0a139d3bc5d3008c0b90eab67
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-24 17:24:01 +0700

    add Icon component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let id
+  export let className
+</script>
+
+<svg width="15" height="15" viewBox="0 0 15 15" fill="none" class={className}>
+  <use href="/static/icons/{id}.svg#{id}" />
+</svg>
