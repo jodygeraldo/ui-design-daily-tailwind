dedb020178cda31d155a15d0b4490905a287a0c0
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-24 17:24:33 +0700

    chore: format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,18 @@
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {
-			colors: {
-				primary: 'var(--color-primary)',
-				ascent: 'var(--color-ascent)',
-				'high-contrast': 'var(--text-color-high)',
-				'low-contrast': 'var(--text-color-low)'
-			}
-		}
-	},
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        ascent: 'var(--color-ascent)',
+        'high-contrast': 'var(--text-color-high)',
+        'low-contrast': 'var(--text-color-low)',
+      },
+    },
+  },
 
-	plugins: []
+  plugins: [],
 }
 
 module.exports = config
