2e33b2acb1587634fef46ae8d056b3af51b813b2
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-28 19:49:41 +0700

    update list

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,21 +1,6 @@
-import { Intl, Temporal } from '@js-temporal/polyfill'
 import january2022List from './2022/january'
-
-function formattedDate(day: number, month: number, year: number) {
-  const dateOptions: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }
-
-  return Intl.DateTimeFormat(undefined, dateOptions).format(
-    Temporal.PlainDate.from({
-      year,
-      month,
-      day,
-    }),
-  )
-}
+import february2022List from './2022/february'
+import march2022List from './2022/march'
 
 export interface ItemType {
   id: number
@@ -26,7 +11,11 @@ export interface ItemType {
   imageUrl: string
 }
 
-const list: ItemType[] = [...january2022List]
+const list: ItemType[] = [
+  ...march2022List,
+  ...february2022List,
+  ...january2022List,
+]
 
-export { january2022List, formattedDate }
+export { january2022List, february2022List, march2022List }
 export default list
