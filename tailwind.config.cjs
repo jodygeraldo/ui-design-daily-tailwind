1fd4700b33a2bf32bb2110a081ee5f7ea4ea65a6
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-28 22:41:52 +0700

    style: change the main page design (#5)
    * fix: background overlaps
    
    * update main page design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,12 +4,25 @@ const config = {
   theme: {
     extend: {
       colors: {
-        app: '#838faa',
         primary: 'var(--color-primary)',
         ascent: 'var(--color-ascent)',
         tertiary: 'var(--color-tertiary)',
         'high-contrast': 'var(--text-color-high)',
         'low-contrast': 'var(--text-color-low)',
+        gray: {
+          1: 'var(--gray1)',
+          2: 'var(--gray2)',
+          3: 'var(--gray3)',
+          4: 'var(--gray4)',
+          5: 'var(--gray5)',
+          6: 'var(--gray6)',
+          7: 'var(--gray7)',
+          8: 'var(--gray8)',
+          9: 'var(--gray9)',
+          10: 'var(--gray10)',
+          11: 'var(--gray11)',
+          12: 'var(--gray12)',
+        },
       },
     },
   },
