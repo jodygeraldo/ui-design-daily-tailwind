cb3a878c68e6bea9fe19b2534aa0e3c04e2c1142
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-28 16:38:16 +0700

    add remaining pending list

--- src/data/2022/february.ts
+++ src/data/2022/february.ts
@@ -31,6 +31,159 @@ export const pendingList: ItemType[] = [
     linkComponent: '/2022/february/3-user-card',
     imageUrl: '3-february-2022.png',
   },
+  {
+    id: 1545,
+    title: 'Subscribe Card',
+    date: formattedDate(4, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-subscribe-card-newsletter-day-1545',
+    linkComponent: '/2022/february/4-subscribe-card',
+    imageUrl: '4-february-2022.png',
+  },
+  {
+    id: 1546,
+    title: 'Details Card',
+    date: formattedDate(7, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-details-card-day-1546',
+    linkComponent: '/2022/february/7-details-card',
+    imageUrl: '7-february-2022.png',
+  },
+  {
+    id: 1547,
+    title: 'Product Details Card',
+    date: formattedDate(8, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-product-details-card-info-pricing-day-1547',
+    linkComponent: '/2022/february/8-product-details-card',
+    imageUrl: '8-february-2022.png',
+  },
+  {
+    id: 1548,
+    title: 'Cancel Plan Modal',
+    date: formattedDate(9, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-cancel-plan-modal-pop-up-day-1548',
+    linkComponent: '/2022/february/9-cancel-plan-modal',
+    imageUrl: '9-february-2022.png',
+  },
+  {
+    id: 1549,
+    title: 'Price Card',
+    date: formattedDate(10, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-price-card-pricing-day-1549',
+    linkComponent: '/2022/february/10-price-card',
+    imageUrl: '10-february-2022.png',
+  },
+  {
+    id: 1550,
+    title: 'Filter',
+    date: formattedDate(11, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-filter-dropdown-tab-day-1550',
+    linkComponent: '/2022/february/11-filter',
+    imageUrl: '11-february-2022.png',
+  },
+  {
+    id: 1551,
+    title: 'Cards',
+    date: formattedDate(14, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-cards-card-menu-day-1551',
+    linkComponent: '/2022/february/14-cards',
+    imageUrl: '14-february-2022.png',
+  },
+  {
+    id: 1552,
+    title: 'Search',
+    date: formattedDate(15, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-search-categories-day-1552',
+    linkComponent: '/2022/february/15-search',
+    imageUrl: '15-february-2022.png',
+  },
+  {
+    id: 1553,
+    title: 'Progress',
+    date: formattedDate(16, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-progress-bar-check-list-to-do-day-1553',
+    linkComponent: '/2022/february/16-progress',
+    imageUrl: '16-february-2022.png',
+  },
+  {
+    id: 1554,
+    title: 'Cards',
+    date: formattedDate(17, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-cards-menu-card-rating-day-1554',
+    linkComponent: '/2022/february/17-cards',
+    imageUrl: '17-february-2022.png',
+  },
+  {
+    id: 1555,
+    title: 'Settings',
+    date: formattedDate(18, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-settings-tab-day-1555',
+    linkComponent: '/2022/february/18-settings',
+    imageUrl: '18-february-2022.png',
+  },
+  {
+    id: 1556,
+    title: 'Contraction Timer',
+    date: formattedDate(21, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-contraction-timer-day-1556',
+    linkComponent: '/2022/february/21-contraction-timer',
+    imageUrl: '21-february-2022.png',
+  },
+  {
+    id: 1557,
+    title: 'Notifications UI Design',
+    date: formattedDate(22, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-notifications-ui-design-day-1557',
+    linkComponent: '/2022/february/22-notifications-ui-design',
+    imageUrl: '22-february-2022.png',
+  },
+  {
+    id: 1558,
+    title: 'Profile',
+    date: formattedDate(23, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-profile-card-day-1558',
+    linkComponent: '/2022/february/23-profile',
+    imageUrl: '23-february-2022.png',
+  },
+  {
+    id: 1559,
+    title: 'Notification',
+    date: formattedDate(24, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-notification-day-1559',
+    linkComponent: '/2022/february/24-notification',
+    imageUrl: '24-february-2022.png',
+  },
+  {
+    id: 1560,
+    title: 'Subscribe',
+    date: formattedDate(25, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-subscribe-dark-mode-day-1560',
+    linkComponent: '/2022/february/25-subscribe',
+    imageUrl: '25-february-2022.png',
+  },
+  {
+    id: 1561,
+    title: 'Cards UI Design',
+    date: formattedDate(28, 2, 2022),
+    linkOriginal:
+      'https://www.uidesigndaily.com/posts/figma-cards-ui-design-card-info-day-1561',
+    linkComponent: '/2022/february/28-cards-ui-design',
+    imageUrl: '28-february-2022.png',
+  },
 ]
 
 export default list
