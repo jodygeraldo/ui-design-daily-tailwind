c3e6a0a9630ae2e37dd1c862dd3d19ad10a584db
Jody Geraldo <ajody.geraldo@gmail.com>
2022-04-01 16:02:06 +0700

    chore: format

--- src/routes/2022/february/17-cards.svelte
+++ src/routes/2022/february/17-cards.svelte
@@ -15,30 +15,30 @@
   <title>Cards - UI Design Daily with Tailwind CSS</title>
 </svelte:head>
 
-<div class="theme grid place-items-center min-h-screen">
-  <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+<div class="theme grid min-h-screen place-items-center">
+  <div class="container mx-auto px-4 py-4 sm:px-6 lg:px-8">
     <div
-      class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 items-start relative"
+      class="relative grid grid-cols-1 items-start gap-10 lg:grid-cols-2 xl:grid-cols-3"
     >
       <div
-        class="hidden 2xl:block absolute inset-x-[38%] -inset-y-1/2 bg-[#E5D8F5] rounded-[80px] col-span-3 -z-10"
+        class="absolute inset-x-[38%] -inset-y-1/2 -z-10 col-span-3 hidden rounded-[80px] bg-[#E5D8F5] 2xl:block"
       />
       <div
-        class="rounded-3xl bg-white p-4 sm:p-8 space-y-6 shadow-[0_10px_7px_0_#15325f10] max-w-md mx-auto col-span-1 2xl:translate-x-52 2xl:-translate-y-24"
+        class="col-span-1 mx-auto max-w-md space-y-6 rounded-3xl bg-white p-4 shadow-[0_10px_7px_0_#15325f10] sm:p-8 2xl:translate-x-52 2xl:-translate-y-24"
       >
         <div class="w-4/5">
-          <div class="flex gap-4 items-center">
+          <div class="flex items-center gap-4">
             <img
               src="/assets/images/trello.png"
               alt=""
-              class="w-14 h-14 rounded-full"
+              class="h-14 w-14 rounded-full"
             />
-            <h2 class="font-black text-2xl text-primary font-serif">Trello</h2>
+            <h2 class="font-serif text-2xl font-black text-primary">Trello</h2>
           </div>
           <div class="flex gap-4">
             <div
               aria-hidden="true"
-              class="shrink-0 w-14 h-14 rounded-full bg-transparent"
+              class="h-14 w-14 shrink-0 rounded-full bg-transparent"
             />
             <p class="font-medium">
               Build on top of Trello by creating a Power-Up.
@@ -50,9 +50,9 @@
           <div class="flex gap-4">
             <div
               aria-hidden="true"
-              class="shrink-0 w-14 h-14 rounded-full bg-transparent"
+              class="h-14 w-14 shrink-0 rounded-full bg-transparent"
             />
-            <ul class="grid grid-cols-1 sm:grid-cols-2 justify-between gap-4">
+            <ul class="grid grid-cols-1 justify-between gap-4 sm:grid-cols-2">
               {#each links as link}
                 <li class="col-span-1">
                   <a href={`#${link}`} class="font-medium text-ascent">{link}</a
@@ -65,7 +65,7 @@
       </div>
 
       <div
-        class="rounded-3xl bg-primary p-4 sm:p-6 space-y-6 shadow-[0_10px_7px_0_#15325f20] max-w-md mx-auto col-span-1 2xl:-translate-x-32 2xl:translate-y-64"
+        class="col-span-1 mx-auto max-w-md space-y-6 rounded-3xl bg-primary p-4 shadow-[0_10px_7px_0_#15325f20] sm:p-6 2xl:-translate-x-32 2xl:translate-y-64"
       >
         <h2 class="font-medium text-white">Rate this page</h2>
         <div class="flex items-center gap-2">
@@ -78,9 +78,9 @@
       </div>
 
       <div
-        class="rounded-3xl bg-white p-4 sm:p-8 space-y-6 shadow-[0_10px_7px_0_#15325f10] max-w-md mx-auto col-span-1 2xl:-translate-x-48 2xl:translate-y-12"
+        class="col-span-1 mx-auto max-w-md space-y-6 rounded-3xl bg-white p-4 shadow-[0_10px_7px_0_#15325f10] sm:p-8 2xl:-translate-x-48 2xl:translate-y-12"
       >
-        <h2 class="ml-[10px] font-black text-2xl text-primary">On this page</h2>
+        <h2 class="ml-[10px] text-2xl font-black text-primary">On this page</h2>
         <div class="space-y-2">
           <a href="#link" class="block p-[10px] font-medium text-primary"
             >Payment models</a
@@ -90,7 +90,7 @@
           >
           <a
             href="#link"
-            class="block p-[10px] font-medium bg-[#f2f6ff] border border-[#d7e1ff] text-ascent "
+            class="block border border-[#d7e1ff] bg-[#f2f6ff] p-[10px] font-medium text-ascent "
           >
             Partner agreements and terms of use
           </a>
