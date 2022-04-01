68dc8fb6afb3f2245e9d113004a0adb042392612
Jody Geraldo <ajody.geraldo@gmail.com>
2022-04-01 19:10:36 +0700

    chore: format

--- src/routes/2022/february/21-contraction-timer.svelte
+++ src/routes/2022/february/21-contraction-timer.svelte
@@ -20,32 +20,32 @@
   <title>Contraction Timer - UI Design Daily with Tailwind CSS</title>
 </svelte:head>
 
-<div class="theme grid place-items-center min-h-screen">
-  <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+<div class="theme grid min-h-screen place-items-center">
+  <div class="container mx-auto px-4 py-4 sm:px-6 lg:px-8">
     <div
-      class="rounded-3xl py-12 px-4 text-primary bg-white max-w-md mx-auto space-y-8 shadow-[0_24px_27px_0_#6971ba16] relative 2xl:mb-24"
+      class="relative mx-auto max-w-md space-y-8 rounded-3xl bg-white py-12 px-4 text-primary shadow-[0_24px_27px_0_#6971ba16] 2xl:mb-24"
     >
       <div
-        class="hidden 2xl:block absolute top-16 -bottom-24 left-1/2 -right-16 bg-primary rounded-[43px] -z-10"
+        class="absolute top-16 -bottom-24 left-1/2 -right-16 -z-10 hidden rounded-[43px] bg-primary 2xl:block"
       />
       <div class="flex items-center justify-between px-5">
         <button>
           <Icon id="arrow-left" className="w-7 h-7" />
         </button>
-        <h2 class="font-medium text-lg">Contraction timer</h2>
+        <h2 class="text-lg font-medium">Contraction timer</h2>
         <button>
           <Icon id="bell" className="w-8 h-8" />
         </button>
       </div>
 
       <div class="text-center">
-        <p class="font-medium text-5xl">00:00:11</p>
+        <p class="text-5xl font-medium">00:00:11</p>
         <div class="mt-6">
           <p class="font-medium">Time since last contractions</p>
           <p class="font-bold">7 min 32 sec</p>
         </div>
         <button
-          class="mt-4 text-white font-bold rounded-full py-6 px-8 bg-primary"
+          class="mt-4 rounded-full bg-primary py-6 px-8 font-bold text-white"
           >Stop timing</button
         >
       </div>
@@ -53,7 +53,7 @@
       <div class="overflow-x-auto">
         <table class="min-w-full tabular-nums">
           <thead class="font-medium opacity-50">
-            <tr class="text-sm sm:text-base text-left">
+            <tr class="text-left text-sm sm:text-base">
               <th scope="col" class="p-3">Length</th>
               <th scope="col" class="p-3">Time apart</th>
               <th scope="col" class="p-3">Start & end time</th>
@@ -67,7 +67,7 @@
                     class={clsx(
                       idx === 0 && 'rounded-l-lg text-center',
                       idx === 2 && 'rounded-r-lg',
-                      'p-4 whitespace-nowrap',
+                      'whitespace-nowrap p-4',
                     )}>{td}</td
                   >
                 {/each}
