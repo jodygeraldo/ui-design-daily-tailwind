dfc9d9170900d6cd27da5fa083ea8c337ed4f089
Jody Geraldo <ajody.geraldo@gmail.com>
2022-04-01 13:34:56 +0700

    migrate to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare'
+import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,6 +13,9 @@ const config = {
 
   kit: {
     adapter: adapter(),
+    prerender: {
+      default: true,
+    },
   },
 }
 
