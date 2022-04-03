777a063b89e0b54af43d06a4cd1b4b5292e15c00
Jody Geraldo <ajody.geraldo@gmail.com>
2022-04-03 13:36:06 +0700

    add aria-hidden props to icons

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -2,11 +2,19 @@
   import type { IconId } from '$lib/IconType'
 
   export let id: IconId
+  export let ariaHidden: boolean = false
   let className: svelte.JSX.SVGAttributes<SVGSVGElement>['class']
 
   export { className as class }
 </script>
 
-<svg width="15" height="15" viewBox="0 0 15 15" fill="none" class={className}>
+<svg
+  width="15"
+  height="15"
+  viewBox="0 0 15 15"
+  fill="none"
+  class={className}
+  aria-hidden={ariaHidden}
+>
   <use href="/assets/icons/{id}.svg#{id}" />
 </svg>
