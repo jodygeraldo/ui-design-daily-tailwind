42c0223af36aeffc333ec55f1c74010ae4ec5008
Jody Geraldo <ajody.geraldo@gmail.com>
2022-04-06 15:45:26 +0700

    chore: format

--- src/routes/2022/march/18-product-info-ui-design.svelte
+++ src/routes/2022/march/18-product-info-ui-design.svelte
@@ -15,41 +15,41 @@
 <div class="theme grid min-h-screen place-items-center">
   <div class="container mx-auto px-4 py-4 sm:px-6 lg:px-8">
     <div
-      class="rounded-2xl bg-white shadow-[0_24px_28px_9px_#b3c1d933] px-6 py-8 lg:px-10 xl:px-12 max-w-3xl mx-auto"
+      class="mx-auto max-w-3xl rounded-2xl bg-white px-6 py-8 shadow-[0_24px_28px_9px_#b3c1d933] lg:px-10 xl:px-12"
     >
-      <div class="sm:flex gap-4 sm:gap-6 md:gap-8 items-start">
+      <div class="items-start gap-4 sm:flex sm:gap-6 md:gap-8">
         <img src="/assets/images/headset.png" alt="" class="w-56" />
 
-        <div class="mt-8 sm:mt-0 space-y-4 sm:space-y-6 flex-1">
-          <div class="bg-custom-1 px-2 py-1 rounded-full inline-block">
-            <p class="text-white font-semibold text-sm">Free shipping</p>
+        <div class="mt-8 flex-1 space-y-4 sm:mt-0 sm:space-y-6">
+          <div class="inline-block rounded-full bg-custom-1 px-2 py-1">
+            <p class="text-sm font-semibold text-white">Free shipping</p>
           </div>
 
-          <h2 class="mt-2 font-semibold text-custom-1 text-2xl">
+          <h2 class="mt-2 text-2xl font-semibold text-custom-1">
             Razer Kraken Kitty Edt Gaming Headset Quartz
           </h2>
 
           <div>
             <s class="text-lg text-custom-1">1 599,-</s>
-            <p class="mt-2 text-custom-1 text-4xl sm:text-5xl">799,-</p>
+            <p class="mt-2 text-4xl text-custom-1 sm:text-5xl">799,-</p>
             <p class="mt-2 text-sm text-custom-1 opacity-60">
               The offer is valid until April 3 or as long as stock lasts!
             </p>
           </div>
 
           <button
-            class="w-full text-white bg-custom-4 p-4 rounded-lg shadow-[0_7px_0_0_#1c57ee]"
+            class="w-full rounded-lg bg-custom-4 p-4 text-white shadow-[0_7px_0_0_#1c57ee]"
             >Add to cart</button
           >
 
-          <div class="flex gap-2 items-center">
-            <div class="w-3 h-3 bg-custom-3 rounded-full" />
-            <p class="text-black text-sm font-medium">50+ pcs. in stock.</p>
+          <div class="flex items-center gap-2">
+            <div class="h-3 w-3 rounded-full bg-custom-3" />
+            <p class="text-sm font-medium text-black">50+ pcs. in stock.</p>
           </div>
 
-          <div class="md:flex gap-4 justify-between items-center">
+          <div class="items-center justify-between gap-4 md:flex">
             <button
-              class="flex justify-center items-center gap-2 w-full p-4 rounded-lg bg-white border-2 border-custom-2"
+              class="flex w-full items-center justify-center gap-2 rounded-lg border-2 border-custom-2 bg-white p-4"
             >
               <svg
                 width="32"
@@ -57,7 +57,7 @@
                 viewBox="0 0 32 32"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                class="w-6 h-6"
+                class="h-6 w-6"
               >
                 <path
                   d="M9.33333 26.6667H22.6667"
@@ -96,20 +96,20 @@
                 />
               </svg>
 
-              <span class="text-custom-1 sm:text-lg font-medium"
+              <span class="font-medium text-custom-1 sm:text-lg"
                 >Add to cart</span
               >
             </button>
 
             <button
-              class="mt-4 md:mt-0 flex justify-center items-center gap-2 w-full p-4 rounded-lg bg-white border-2 border-custom-2"
+              class="mt-4 flex w-full items-center justify-center gap-2 rounded-lg border-2 border-custom-2 bg-white p-4 md:mt-0"
             >
               <Icon
                 id="heart"
-                class="w-6 h-6 text-custom-1"
+                class="h-6 w-6 text-custom-1"
                 ariaHidden={true}
               />
-              <span class="text-custom-1 sm:text-lg font-medium"
+              <span class="font-medium text-custom-1 sm:text-lg"
                 >Add to cart</span
               >
             </button>
